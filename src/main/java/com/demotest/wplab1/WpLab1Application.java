870da9755ef64ef2d1a595b76ded9b883acdad27
Hristijan Mijalkov <hristijan-mijalkov5@vista-point.io>
2024-10-26 10:29:21 +0200

    Initial commit

---
+++ src/main/java/com/demotest/wplab1/WpLab1Application.java
@@ -0,0 +1,13 @@
+package com.demotest.wplab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpLab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/demotest/wplab1/WpLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demotest.wplab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
